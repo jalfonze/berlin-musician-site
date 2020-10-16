2419689c9d3d1f5677e8ccedc0e2fdb9cf191ee6
Justin Alfonso <justinalfonso29@hotmail.com>
2020-10-16 12:29:34 +0200

    HEROKU

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const spicedPg = require("spiced-pg");
 
-const db = spicedPg("postgres:postgres:postgres@localhost:5432/food-user");
+const db = spicedPg(
+    process.env.DATABASE_URL ||
+        "postgres:postgres:postgres@localhost:5432/food-user"
+);
 
 module.exports.createUser = (username, email, img, pw) => {
     return db.query(
