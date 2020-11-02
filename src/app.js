dd8020e6942caa42ed22d5c05b5257d4e6928b22
Justin Alfonso <justinalfonso29@hotmail.com>
2020-11-02 23:55:11 +0100

    Template

--- src/app.js
+++ src/app.js
@@ -1,53 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
-import Profile from "./profile";
-import Search from "./search";
-import MyFavourites from "./my-favourites";
-import MyRecipes from "./my-recipes";
 import axios from "./axios";
 
 export default function App() {
-    const logOut = () => {
-        axios.get("/logout");
-    };
-
     return (
         <React.Fragment>
-            <BrowserRouter>
-                <div className="nav">
-                    <img src="/logo.png" width="100px"></img>
-                    <h5>
-                        <Link to="/">
-                            <i className="fas fa-user"></i>
-                        </Link>
-                    </h5>
-                    <h5>
-                        <Link to="/search">
-                            <i className="fas fa-search"></i>
-                        </Link>
-                    </h5>
-                    <h5>
-                        <Link to="/my-fave">
-                            <i className="fas fa-bookmark"></i>
-                        </Link>
-                    </h5>
-                    <h5>
-                        <Link to="/my-recipes">
-                            <i className="fas fa-book"></i>
-                        </Link>
-                    </h5>
-                    <h5>
-                        <a href="/welcome" onClick={logOut}>
-                            <i className="fas fa-sign-out-alt"></i>
-                        </a>
-                    </h5>
-                </div>
-
-                <Route exact path="/" render={() => <Profile />} />
-                <Route path="/search" render={() => <Search />} />
-                <Route path="/my-fave" render={() => <MyFavourites />} />
-                <Route path="/my-recipes" render={() => <MyRecipes />} />
-            </BrowserRouter>
+            <h1>Hello World</h1>
         </React.Fragment>
     );
 }
--- src/start.js
+++ src/start.js
@@ -1,27 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import reduxPromise from "redux-promise";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider } from "react-redux";
-import Reducer from "./reducers";
-import Welcome from "./welcome";
 import App from "./app";
 
-const store = createStore(
-    Reducer,
-    composeWithDevTools(applyMiddleware(reduxPromise))
-);
-
-let component;
-if (location.pathname === "/welcome") {
-    component = <Welcome />;
-} else {
-    component = (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
-}
-
-ReactDOM.render(component, document.querySelector("main"));
+ReactDOM.render(<App />, document.querySelector("main"));
